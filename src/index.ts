4480dda577629cbb28796389a83cfb56f4ffefd6
Stewart <44583600+stewheart123@users.noreply.github.com>
2023-06-05 22:34:55 +0100

    signal dispatch example working

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { Application, Sprite, TextStyle, Text } from 'pixi.js';
-import { Bank} from './models/bank';
+import { Application, Sprite, TextStyle, Text, Graphics } from 'pixi.js';
+import { Bank } from './models/bank';
 import { InitialiseGameState } from './states/InitialiseGameState';
 
 const app = new Application({
@@ -13,18 +13,30 @@ const app = new Application({
 
 const clampy: Sprite = Sprite.from("clampy.png");
 const initialiseGameState: InitialiseGameState = new InitialiseGameState();
-let bank :Bank = new Bank();
+let bank: Bank = new Bank();
 bank.balance = initialiseGameState.addCredit();
+initialiseGameState.setGameReady();
 const txtStyle: TextStyle = new TextStyle({
 	fontSize: 50,
 	fill: 0xffffff,
 });
-const text :Text = new Text("Bank Balance", txtStyle);
+const text: Text = new Text("Bank Balance", txtStyle);
 text.text = "test";
 
+const menuButton = new Graphics();
+menuButton.beginFill(0x000000, 0.5);
+menuButton.lineStyle(2, 0xffffff, 4);
+menuButton.drawRect(0, 0, 500, 500);
+menuButton.endFill();
+menuButton.interactive = true;
+menuButton.cursor = "pointer";
 
+menuButton.addListener("pointerdown", () => {
+	initialiseGameState.clickSignal.dispatch();
+});
+app.stage.addChild(menuButton);
 
-clampy.anchor.set(0.5);
+clampy.anchor.set(0.5)
 
 clampy.x = app.screen.width / 2;
 clampy.y = app.screen.height / 2;
